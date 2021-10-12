a66b06ff06452fd7bb570a21187ec57da2d011e8
camelcrush <dkxm0927@naver.com>
2021-10-13 00:58:44 +0900

    last version

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from . import managers
+
+
+class TimeStampedModel(models.Model):
+
+    """Time Stamped Model"""
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    objects = managers.CustomManager()
+
+    class Meta:
+        abstract = True
---
+++ users/models.py
@@ -0,0 +1,93 @@
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.db import models
+from django.core.mail import send_mail
+from django.utils.html import strip_tags
+from django.shortcuts import reverse
+from django.template.loader import render_to_string
+from core import managers as core_managers
+from django.utils.translation import gettext_lazy as _
+import uuid
+
+
+class User(AbstractUser):
+
+    """Custom User Model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMANLE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, _("Male")),
+        (GENDER_FEMANLE, _("Female")),
+        (GENDER_OTHER, _("Other")),
+    )
+
+    LANGUAGE_ENGLISH = "eg"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, _("Eglish")),
+        (LANGUAGE_KOREAN, _("Korean")),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_GITHUB, "Github"),
+        (LOGIN_KAKAO, "Kakao"),
+    )
+
+    avatar = models.ImageField(upload_to="avatars", blank=True)
+    gender = models.CharField(
+        _("gender"), choices=GENDER_CHOICES, max_length=10, blank=True
+    )
+    bio = models.TextField(_("bio"), blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(
+        _("language"),
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        blank=True,
+        default=LANGUAGE_KOREAN,
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW
+    )
+    superhost = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+    email_secret = models.CharField(max_length=120, default="", blank=True)
+    login_method = models.CharField(
+        max_length=80, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
+    objects = core_managers.CustomUserManager()
+
+    def verify_email(self):
+        if self.email_verified is False:
+            secret = uuid.uuid4().hex[:20]
+            self.email_secret = secret
+            html_message = render_to_string(
+                "emails/verify_email.html", {"secret": secret}
+            )
+            send_mail(
+                _("Verify Your Account"),
+                strip_tags(html_message),
+                settings.EMAIL_FROM,
+                [self.email],
+                fail_silently=False,
+                html_message=html_message,
+            )
+            self.save()
+        return
+
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={"pk": self.pk})
